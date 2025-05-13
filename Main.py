8e5446e02104f31eb62f72fa83af619dd08d6005
Ahmad Zeeshan <itzahmad292@gmail.com>
2025-05-13 22:38:49 +0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,29 @@
+import cv2
+from AutonomousVehicle import SelfDrivingVehicle
+from ObjectDetector import ObjectDetection
+
+if __name__ == "__main__":
+    path = r'C:\Users\Ahmad Zeeshan\OneDrive\Desktop\semestor\6th semestor\DIP\Project\Dataset\DIP Project Videos\PXL_20250325_043754655.TS.mp4'
+    video = cv2.VideoCapture(path)
+
+    ret, frame = video.read()
+    scaling_factor = 1
+    frame = cv2.resize(frame, (int(1280 * scaling_factor), int(720 * scaling_factor)), interpolation=cv2.INTER_AREA)
+    obj = ObjectDetection(frame, scaling_factor)
+
+    car = SelfDrivingVehicle(frame, scaling_factor)
+
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, (int(1280 * scaling_factor), int(720 * scaling_factor)), interpolation=cv2.INTER_AREA)
+        car_o = car.forward(frame)
+
+        cv2.imshow("output", car_o)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
